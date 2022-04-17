1aa34cc6812382e34454d57eacfc03dd4c7520e0
Jose Anthony Jovellanos <jove0610@gmail.com>
2022-04-17 15:10:45 +0800

    include native node modules

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ class ForgeExternalsPlugin {
         walker.modules = [];
         await walker.walkDependenciesForModule(moduleRoot, DepType.PROD);
         walker.modules
-          .filter((dep) => dep.nativeModuleType === DepType.PROD)
           .map((dep) => dep.name)
           .forEach((name) => foundModules.add(name));
       }
